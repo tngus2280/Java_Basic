78bde206aca6af2fbe9a21a78a9034750b51453d
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-04-05 20:28:38 +0900

    홀수 짝수 판별 코드

---
+++ java04_control/condition/IfElse_02.java
@@ -0,0 +1,17 @@
+package java04_control.condition;
+
+public class IfElse_02 {
+	public static void main(String[] args) {
+	
+		int num = 7;
+		
+		//홀수 짝수 판별 코드 작성해보세요!
+		
+		if(num%2 ==0) {
+			System.out.println("짝수입니다");
+		}else{
+			System.out.println("홀수입니다");
+		}
+		
+	}
+}
