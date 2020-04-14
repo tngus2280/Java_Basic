624ef8b7ef28e9ae4ad98aec5ee5b0d4c7ca755d
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-04-14 20:29:00 +0900

    while문

---
+++ java04_control/loop/While_02.java
@@ -0,0 +1,24 @@
+package java04_control.loop;
+
+public class While_02 {
+	public static void main(String[] args) {
+		
+		int i=1;
+		while ( i<=10) {
+			System.out.println(i);
+			
+			i++;
+		}
+		
+		System.out.println("-------------");
+		
+		int j=1;
+		for ( ;i<=10;) {
+			System.out.println(i);
+			
+			j++;
+		
+		}	
+		
+	}
+}
