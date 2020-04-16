2ebe5cb7f8a9620078d3f028fcb857eef7b98841
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-04-16 19:04:29 +0900

    별찍기 2번

---
+++ java04_control/star/Star_02.java
@@ -0,0 +1,24 @@
+package java04_control.star;
+
+public class Star_02 {
+	public static void main(String[] args) {
+		
+		//Q2
+		// *****
+		
+		//반복횟수: 5
+		//반복구간: 0~4
+				
+				
+		// 초기식: 		int i = 0;				(반복구간의 시작값)
+		// 조건식: 	    i<5;  		더한값? 			(반복구간의 끝값, 반복횟수)
+		// 증감식: 		i++						(1씩 증가)	
+			
+		
+		for(int i =0; i<5; i++) { // i , 0~4 ,x5 
+			System.out.println("*");
+		}
+		
+	
+	}
+}
