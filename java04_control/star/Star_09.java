6031790a5b0703555171a0be12742457f6477659
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-04-21 15:42:06 +0900

    별찍기 9번

---
+++ java04_control/star/Star_09.java
@@ -0,0 +1,13 @@
+package java04_control.star;
+
+public class Star_09 {
+	public static void main(String[] args) {
+		
+	for(int i =0; i<5; i++) { // i , 5~1 ,x5 
+		for(int j= i+1; j<=5;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+}
+}
+}
