aa97881450bfde3fe512dab473b0cc14a79c5293
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-05-13 19:39:25 +0900

    멤버필드

---
+++ java06_class/field/Memberfield_01.java
@@ -0,0 +1,26 @@
+package java06_class.field;
+// 기능 클래스 ,인스턴스변수
+public class MemberField_01 {
+	
+	
+	//멤버 필드
+	private int num = 999;
+	private String name = "Alice";
+	
+	// Generate Getters and Setters 단축키 : alt + Shift + s  
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+		
+	
+}
