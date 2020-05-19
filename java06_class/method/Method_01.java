6e453f08058a50e7cfc7ae69509dfb41e6fc3271
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-05-19 15:33:57 +0900

    메소드

---
+++ java06_class/method/Method_01.java
@@ -0,0 +1,29 @@
+package java06_class.method;
+
+public class Method_01 { //기능 클래스
+		
+	// 0. 메소드의 기본형태 작성												-> public void 
+	// 1. 메소드 이름 -> 만들고자 하는 기능을 대표하는 이름으로 적절히  		->덧셈기능 add
+	// 2. 매개변수 작성 -> 기능을 수행하는 데 필요한 입력 값을 저장하는 변수 	-> 정수 2개 , int num1 , int num2
+	// 3. 실행코드 작성 -> 메소드가 수행할 기능을 작성, 구현					-> num1 + num2
+	// 4. return코드 작성 -> 수행 결과로 사용할 반환 값 작성					-> return result;
+	// 5. return 데이터타입 작성 -> return 코드 값의 데이터 타입을 명시			-> int		
+	// 2개의 정수를 덧셈해주는 기능
+		public int add(int num1 , int num2) { //덧셈 기능 , add
+		
+			int result = num1 + num2; 
+			
+			return result;  //일시정지된 코드에 반환
+			
+			
+			
+			
+			
+			
+			
+			
+		
+	
+		}
+}
+
