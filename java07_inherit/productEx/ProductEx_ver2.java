2acd61c3069afb2ac3725218123f5a6af8bb5e40
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-06-02 20:33:41 +0900

    ver2

---
+++ java07_inherit/productEx/ProductEx_ver2.java
@@ -0,0 +1,35 @@
+package java07_inherit.productEx;
+
+import java07_inherit.person.Person_ver2;
+import java07_inherit.product.Computer;
+import java07_inherit.product.Tv;
+
+public class ProductEx_ver2 {
+	public static void main(String[] args) {
+
+		Person_ver2 p = new Person_ver2("Alice", 5000);
+		
+		p.sell();
+		p.buy( new Tv("LG1", 1000) );
+		p.buy( new Tv("SAMSUNG1", 700) );
+		p.buy( new Computer("i5", 500) );
+		p.buy( new Tv("SAMSUNG2", 800) );
+		System.out.println("----------");
+		p.sell();
+		System.out.println("----------");
+		p.sell();
+		System.out.println("----------");
+		p.sell();
+
+		p.buy( new Computer("i5", 500) );
+		p.buy( new Tv("SAMSUNG2", 800) );
+		p.buy( new Tv("LG1", 1000) );
+		p.buy( new Tv("SAMSUNG2", 800) );
+
+		System.out.println("----------");
+		p.sell();
+		System.out.println("----------");
+		p.sell();
+
+	}
+}
