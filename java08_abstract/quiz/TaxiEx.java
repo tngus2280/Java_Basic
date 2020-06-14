9ae4f836d13c17cd440d029c3d34654e844e3a34
tngus2280 <62205408+tngus2280@users.noreply.github.com>
2020-06-14 23:22:45 +0900

    TaxiEx

---
+++ java08_abstract/quiz/TaxiEx.java
@@ -0,0 +1,18 @@
+package java08_abstract.interfaceEx.quiz;
+
+public class TaxiEx {
+	public static void main(String[] args) {
+		Taxi taxi = new Taxi(); 
+		
+		//요금 = 거리 x 비용 x2
+		taxi.calc(2500);
+		
+		//출력
+		// calc() 메소드를 이용하여 출력한다
+		
+		// 2500m를 운행했을때 요금은 xxx원 입니다
+		
+		
+		
+	}
+}
