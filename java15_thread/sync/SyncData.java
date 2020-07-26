a8db90aac693c5368481605bbfed69cd616612cf
KimSuHyeon <tngus2280@naver.com>
2020-07-26 16:07:53 +0900

    SyncData

---
+++ java15_thread/sync/SyncData.java
@@ -0,0 +1,11 @@
+package java15_thread.sync;
+
+public class SyncData { // 데이터 저장소
+	int total; // 덧셈 결과
+	
+	boolean available = false; //flag
+	// 	true -덧셈이 이루어진 후
+	//	false - 덧셈이 이루어지기 전
+	
+	
+}
